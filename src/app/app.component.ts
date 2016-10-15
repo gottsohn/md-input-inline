0d84e5b5bf4cbd198a80e51979552980d2c28fc5
Godson Ukpere <godson.ukpere@gmail.com>
2016-10-15 09:46:31 +0100

    chore(User): Create user model for Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  user: User = new User('Godson', 'vor.nachname@gmail.com', '+234-809-613-2999');
+  constructor() {}
 }
