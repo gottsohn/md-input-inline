07b1ba98d8d1d8f61531b5ca08b715171cb41447
Godson Ukpere <godson.ukpere@gmail.com>
2016-10-15 19:21:00 +0100

    chore(src) Move src files to /srco directory

---
+++ src/md-input-inline.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+import { MdInputInlineComponent } from './md-input-inline.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule.forRoot()
+  ],
+  declarations: [
+    MdInputInlineComponent,
+  ],
+  exports: [
+    MdInputInlineComponent
+  ]
+})
+
+export class MdInputInlineModule { }
