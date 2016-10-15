d17479d6454368a7f0582781c887676f677ebf1c
Godson Ukpere <godson.ukpere@gmail.com>
2016-10-15 08:43:19 +0100

    chore(md-input-inline): Import component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MdInputInlineComponent } from './components/md-input-inline/md-input-inline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MdInputInlineComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
